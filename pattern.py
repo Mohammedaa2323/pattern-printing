b74fe029d52b8a52a233b92b34b08e7f253fe583
mohammed aa <mohammedaa751055@gmail.com>
2024-07-13 00:36:48 +0530

    final commit

---
+++ pattern.py
@@ -0,0 +1,22 @@
+
+x= int(input("enter number of rows : "))
+y= int(input("enter number of columns : "))
+
+
+
+print(" ",end="")
+for i in range(1,(x*2)+2):
+    for j in range (1,y+1):
+        if i ==1 and j%2!=0:
+            print("___",end="")
+        elif i ==1 and j%2==0:
+            print("     ",end="")
+        elif i %2!=0 and j%2!=0:
+            print("\\___/",end="")
+        elif i%2!=0 and j%2==0:
+            print("   ",end="")    
+        elif i%2==0 and j%2!=0:
+            print("/   \\",  end="")
+        elif i%2==0 and j%2==0:
+            print("___",end="")
+    print()
